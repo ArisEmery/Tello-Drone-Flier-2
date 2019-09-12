96aa432dbc21fd3834c50474fe1d4e4b4df364bd
Aris <aris.emery@gmail.com>
2019-09-11 21:05:16 -0600

    buit the launch mission protocol

--- cs5700/Tello Drone Flier/src/com/company/Flier.java
+++ cs5700/Tello Drone Flier/src/com/company/Flier.java
@@ -35,14 +35,79 @@ public class Flier {
         }
     }
 
-    public void launchMisson(Mission mission){
+    public void launchMisson(Mission mission) throws IOException {
+        //Supported messages: command, takeoff, land, up, down, left, right, forward, back, cw, cww,
+        //stop, speed?, battery?, and time?
+        initiateCommandMode();
         Iterator<String> iter = mission.missionCommands.iterator();
         while (iter.hasNext()) {
-            switch (iter.next()){
-                case "takeoff":
-                    System.out.println("sd");
+            Message nextMessageToSend=null;
+            String currentCommand=iter.next();
+            int unitOfDegreesOrDistance=0;
+            String[] specialString = currentCommand.split(" ");
+            if(specialString.length>1){
+                unitOfDegreesOrDistance=Integer.parseInt(specialString[1]);
             }
-            System.out.print(iter.next() + ", ");
+
+            switch (specialString[0]){
+                case "takeoff":
+                    System.out.println("Taking off...");
+                    nextMessageToSend=new TakeoffMessage();
+                    break;
+                case "land":
+                    System.out.println("Landing...");
+                    nextMessageToSend=new LandMessage();
+                    break;
+                case "up":
+                    System.out.println("Going up by "+unitOfDegreesOrDistance +"cm...");
+                    nextMessageToSend=new UpMessage(unitOfDegreesOrDistance);
+                    break;
+                case "down":
+                    System.out.println("Going down by "+unitOfDegreesOrDistance +"cm...");
+                    nextMessageToSend=new DownMessage(unitOfDegreesOrDistance);
+                    break;
+                case "left":
+                    System.out.println("Going left by "+unitOfDegreesOrDistance +"cm...");
+                    nextMessageToSend=new LeftMessage(unitOfDegreesOrDistance);
+                    break;
+                case "forward":
+                    System.out.println("Going forward by "+unitOfDegreesOrDistance +"cm...");
+                    nextMessageToSend=new ForwardMessage(unitOfDegreesOrDistance);
+                    break;
+                case "back":
+                    System.out.println("Going back by "+unitOfDegreesOrDistance +"cm...");
+                    nextMessageToSend=new BackMessage(unitOfDegreesOrDistance);
+                    break;
+                case "cw":
+                    System.out.println("Rotating clockwise by "+unitOfDegreesOrDistance +"degrees...");
+                    nextMessageToSend=new RotateClockwiseMessage(unitOfDegreesOrDistance);
+                    break;
+                case "cww":
+                    System.out.println("Going counterclockwise by "+unitOfDegreesOrDistance +"degrees...");
+                    nextMessageToSend=new RotateCounterClockwiseMessage(unitOfDegreesOrDistance);
+                    break;
+                case "stop":
+                    System.out.println("Stopping...");
+                    nextMessageToSend=new StopMessage();
+                    break;
+                case "speed?":
+                    System.out.println("Checking speed...");
+                    nextMessageToSend=new SpeedMessage();
+                    break;
+                case "battery?":
+                    System.out.println("Checking battery...");
+                    nextMessageToSend=new BatteryMessage();
+                    break;
+                case "time?":
+                    System.out.println("Checking time...");
+                    nextMessageToSend=new TimeMessage();
+                    break;
+            }
+            udpCommunicator.sendMessage(nextMessageToSend);
+            String reply = null;
+            reply=udpCommunicator.receiveMessage();
+
+
         }
     }
 
@@ -54,45 +119,51 @@ public class Flier {
         //-------- Put drone in command mode ---------
         String droneAddress="127.0.0.1";
         int dronePort=8889;
-//        RotateClockwiseMessage upMessage = new RotateClockwiseMessage(361);
-//        System.out.println(upMessage.isValid());
-//        System.out.println("Welcome to the Telu Edu Drone Flier!");
-//        System.out.println("Would you like to specify the IP Address and Command Port for\n" +
-//                "the drone? (The default drone Address will be 127.0.0.1 and the default\n" +
-//                "port will be 8889. Press 1 for yes and 2 for no (default).");
-//        Scanner scanner = new Scanner(System.in);
-//        int userChoice = scanner.nextInt();
-//        scanner.nextLine();
-//        if (userChoice == 1){
-//            System.out.println("please specify your desired IP address: ");
-//            droneAddress = scanner.nextLine();
-//            System.out.println("please specify your desired command port: ");
-//            dronePort = scanner.nextInt();
-//        }
-//        ArrayList<String> mission = new ArrayList<String>();
-//        mission.add("takeoff");
-//        mission.add("rotate 360");
-//        mission.add("land");
+        ArrayList<String> mission = new ArrayList<String>();
+        mission.add("takeoff");
+        mission.add("up 300");
+        mission.add("land");
+        System.out.println("Welcome to the Telu Edu Drone Flier!");
+        System.out.println("Would you like to specify the IP Address and Command Port for\n" +
+                "the drone? (The default drone Address will be 127.0.0.1 and the default\n" +
+                "port will be 8889. Press 1 for yes and 2 for no (default).");
+        Scanner scanner = new Scanner(System.in);
+        int userChoice = scanner.nextInt();
+        scanner.nextLine();
+        if (userChoice == 1){
+            System.out.println("please specify your desired IP address: ");
+            droneAddress = scanner.nextLine();
+            System.out.println("please specify your desired command port: ");
+            dronePort = scanner.nextInt();
+        }
 
-//        Mission myMission = new Mission(mission);
+
         Flier flier = new Flier(droneAddress,dronePort);
-        flier.initiateCommandMode();
-//        flier.launchMisson(myMission);
+
+        Mission myMission = new Mission(mission);
+        flier.launchMisson(myMission);
+
+
+
+
+
+
+
 //
 //
-        TakeoffMessage takeoffMessage = new TakeoffMessage();
-        String reply = null;
-        flier.udpCommunicator.sendMessage(takeoffMessage);
-        reply=flier.udpCommunicator.receiveMessage();
-        RotateClockwiseMessage rotateClockwiseMessage = new RotateClockwiseMessage(360);
-        reply = null;
-        flier.udpCommunicator.sendMessage(rotateClockwiseMessage);
-        reply=flier.udpCommunicator.receiveMessage();
-        LandMessage landMessage = new LandMessage();
-        reply = null;
-        flier.udpCommunicator.sendMessage(landMessage);
-        reply=flier.udpCommunicator.receiveMessage();
-        System.out.println("Reply: "+reply);
+//        TakeoffMessage takeoffMessage = new TakeoffMessage();
+//        String reply = null;
+//        flier.udpCommunicator.sendMessage(takeoffMessage);
+//        reply=flier.udpCommunicator.receiveMessage();
+//        RotateClockwiseMessage rotateClockwiseMessage = new RotateClockwiseMessage(360);
+//        reply = null;
+//        flier.udpCommunicator.sendMessage(rotateClockwiseMessage);
+//        reply=flier.udpCommunicator.receiveMessage();
+//        LandMessage landMessage = new LandMessage();
+//        reply = null;
+//        flier.udpCommunicator.sendMessage(landMessage);
+//        reply=flier.udpCommunicator.receiveMessage();
+//        System.out.println("Reply: "+reply);
 
 
 
